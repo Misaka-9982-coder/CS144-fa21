0e0c6eb25882838f626af8c0458717ea209af9c7
misaka <1784321894@qq.com>
2023-02-12 15:02:59 +0000

    CS144 Lab 1 stream_reassembler

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -12,15 +12,178 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
-StreamReassembler::StreamReassembler(const size_t capacity) : _output(capacity), _capacity(capacity) {}
+StreamReassembler::StreamReassembler(const size_t capacity)
+    : _output(capacity)
+    , _capacity(capacity)
+    , _first_uass(0)
+    , _unassembled_bytes(0)
+    , _eof(false)
+    , _eof_idx(0)
+    , _blocks() {}
 
 //! \details This function accepts a substring (aka a segment) of bytes,
 //! possibly out-of-order, from the logical stream, and assembles any newly
 //! contiguous substrings and writes them into the output stream in order.
 void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof) {
-    DUMMY_CODE(data, index, eof);
+    // the data that have been reassembled
+    if (index + data.size() < _first_uass) {
+        return;
+    }
+
+    if (eof && !_eof) {
+        _eof = true;
+        _eof_idx = index + data.size();
+    }
+
+    StreamBlock blk(index, move(string(data)));
+
+    // if a part of the data have been reassembled
+    if (index < _first_uass) {
+        blk.buffer().remove_prefix(_first_uass - index);
+    }
+
+    // if a part of the data out of the capacity
+    if (index + data.size() > _capacity + _first_uass) {
+        blk.buffer().remove_suffix(index + data.size() - _capacity - _first_uass);
+    }
+
+    add_block(blk);
+    write_to_stream();
+    EOFcheck();
 }
 
-size_t StreamReassembler::unassembled_bytes() const { return {}; }
+void StreamReassembler::push_substring(const Buffer &data, const size_t index, const bool eof) {
+    // the data that have been reassembled
+    if (index + data.size() < _first_uass) {
+        return;
+    }
 
-bool StreamReassembler::empty() const { return {}; }
+    if (eof && !_eof) {
+        _eof = true;
+        _eof_idx = index + data.size();
+    }
+
+    StreamBlock blk(index, data);
+
+    // if a part of the data have been reassembled
+    if (index < _first_uass) {
+        blk.buffer().remove_prefix(_first_uass - index);
+    }
+
+    // if a part of the data out of the capacity
+    if (index + data.size() > _capacity + _first_uass) {
+        blk.buffer().remove_suffix(index + data.size() - _capacity - _first_uass);
+    }
+
+    add_block(blk);
+    write_to_stream();
+    EOFcheck();
+}
+
+//! \details This function check if eof is written to the stream
+inline void StreamReassembler::EOFcheck() {
+    if (!_eof) {
+        return; 
+    }
+    
+    if (static_cast<size_t>(_eof_idx) == _first_uass) {
+        _output.end_input();
+    }
+}
+
+//! \details This function write the first block into the stream,
+//! the first block should begin at '_first_uass'
+inline void StreamReassembler::write_to_stream() {
+    while (!_blocks.empty()) {
+        auto block = *_blocks.begin();
+        if (block.begin() != _first_uass) {
+            return;
+        }
+
+        size_t bytes_written = _output.write(block.buffer());
+
+        if (bytes_written == 0) {
+            return;
+        }
+
+        _first_uass += bytes_written;
+        _unassembled_bytes -= bytes_written;
+        _blocks.erase(_blocks.begin());
+
+        // partially written
+        if (bytes_written != block.len()) {
+            block.buffer().remove_prefix(bytes_written);
+            _blocks.insert(block);
+        }
+    }
+}
+
+//! \details This function 
+inline void StreamReassembler::add_block(StreamBlock &new_block) {
+    if (new_block.len() == 0) {
+        return;
+    }
+
+    vector<StreamBlock> blks_to_add;
+    blks_to_add.emplace_back(new_block);
+
+    do {
+        if (_blocks.empty()) {
+            break;
+        }
+
+        auto nblk = blks_to_add.begin();
+        auto iter = _blocks.lower_bound(*nblk);
+        auto prev = iter;
+
+        while (iter != _blocks.end() && overlap(*iter, *nblk)) {
+            if ((*iter).end() >= (*nblk).end()) {
+                (*nblk).buffer().remove_suffix((*nblk).end() - (*iter).begin());
+                break;
+            } else {
+                StreamBlock last(*nblk);
+                (*nblk).buffer().remove_suffix((*nblk).end() - (*iter).begin());
+                last.buffer().remove_prefix((*iter).end() - (*nblk).begin());
+                blks_to_add.push_back(last);
+                nblk = blks_to_add.end();
+                nblk -- ;
+                iter ++ ;
+            }
+        }
+
+        // compare with prevs
+        //  check one previous block is enough
+        if (prev == _blocks.begin()) {
+            break;
+        }
+
+        prev -- ;
+        nblk = blks_to_add.begin();
+        
+        if (overlap(*nblk, *prev)) {
+            (*nblk).buffer().remove_prefix((*prev).end() - (*nblk).begin());
+        }
+    } while (false);
+
+    for (auto &blk : blks_to_add) {
+        if (blk.len() != 0) {
+            _blocks.emplace(blk);
+            _unassembled_bytes += blk.len();
+        }
+    }
+}
+
+//! \details This function check if the two blocks have overlap part
+bool StreamReassembler::overlap(const StreamBlock &blk, const StreamBlock &new_blk) const {
+    if (blk.begin() < new_blk.begin()) {
+        return overlap(new_blk, blk);
+    }
+
+    return !(blk.begin() >= new_blk.end());
+}
+
+uint64_t StreamReassembler::first_unassembled() const { return _first_uass; }
+
+size_t StreamReassembler::unassembled_bytes() const { return _unassembled_bytes; }
+
+bool StreamReassembler::empty() const { return _unassembled_bytes == 0; }
--- libsponge/stream_reassembler.hh
+++ libsponge/stream_reassembler.hh
@@ -2,9 +2,34 @@
 #define SPONGE_LIBSPONGE_STREAM_REASSEMBLER_HH
 
 #include "byte_stream.hh"
+#include "buffer.hh"
 
 #include <cstdint>
 #include <string>
+#include <set>
+
+class StreamBlock {
+  private:
+    BufferPlus _buffer{};
+    size_t _begin_index;
+
+  public:
+    StreamBlock(const int begin, std::string &&str) noexcept
+      : _buffer(std::move(str)), _begin_index(begin) {};
+
+    StreamBlock(const StreamBlock &Other) noexcept
+      : _buffer(Other._buffer), _begin_index(Other._begin_index) {};
+
+    StreamBlock(const int begin, const Buffer &data) noexcept
+      : _buffer(data), _begin_index(begin) {};
+
+    bool operator<(const StreamBlock sb) const { return begin() < sb.begin(); }
+    inline size_t end() const { return _begin_index + _buffer.starting_offset() + _buffer.size(); }
+    inline size_t len() const { return _buffer.size(); }
+    inline size_t begin() const { return _begin_index + _buffer.starting_offset(); }
+    BufferPlus &buffer() { return _buffer; }
+    const BufferPlus &buffer() const { return _buffer; }
+};
 
 //! \brief A class that assembles a series of excerpts from a byte stream (possibly out of order,
 //! possibly overlapping) into an in-order byte stream.
@@ -14,6 +39,29 @@ class StreamReassembler {
 
     ByteStream _output;  //!< The reassembled in-order byte stream
     size_t _capacity;    //!< The maximum number of bytes
+    size_t _first_uass;  // index of segment waiting for
+    size_t _unassembled_bytes;
+    bool _eof;           // whether _eof_ is effecitve
+    size_t _eof_idx;     // where the eof is
+    std::set<StreamBlock> _blocks;
+
+    //! Merge the two blocks "blk" and "new_block"
+    //! the result will stored in new_block
+    //! nothing happens if two blocks can't merge
+    //! return ture if merge happens, false otherwise
+
+    //! add "to_add" blocks to set blocks
+    //! merge all the blocks mergeable
+    inline void add_block(StreamBlock &new_block);
+
+    bool overlap(const StreamBlock &blk, const StreamBlock &new_blk) const;
+
+    //! Write the first block to the stream, this block should begin at  '_first_uass'
+    inline void write_to_stream();
+
+    //! Check if eof is written to the stream
+    //! If true, end the stream
+    inline void EOFcheck();
 
   public:
     //! \brief Construct a `StreamReassembler` that will store up to `capacity` bytes.
@@ -30,12 +78,15 @@ class StreamReassembler {
     //! \param index indicates the index (place in sequence) of the first byte in `data`
     //! \param eof the last byte of `data` will be the last byte in the entire stream
     void push_substring(const std::string &data, const uint64_t index, const bool eof);
+    void push_substring(const Buffer &data, const size_t index, const bool eof);
 
     //! \name Access the reassembled byte stream
     //!@{
     const ByteStream &stream_out() const { return _output; }
     ByteStream &stream_out() { return _output; }
     //!@}
+
+    uint64_t first_unassembled() const;
 
     //! The number of bytes in the substrings stored but not yet reassembled
     //!
