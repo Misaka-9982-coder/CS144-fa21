76799323f78859195b2a398466c17ea3befbd2f6
misaka <1784321894@qq.com>
2023-02-13 11:10:19 +0000

    CS144 Lab 2 tcp_receiver

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -1,19 +1,54 @@
 #include "tcp_receiver.hh"
+#include "util.hh"
 
 // Dummy implementation of a TCP receiver
 
 // For Lab 2, please replace with a real implementation that passes the
 // automated checks run by `make check_lab2`.
 
-template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
-
 using namespace std;
 
 void TCPReceiver::segment_received(const TCPSegment &seg) {
-    DUMMY_CODE(seg);
+    if (!_ackno.has_value() && !seg.header().syn) {
+        return;
+    } 
+    
+    // Handshake
+    if (!_ackno.has_value() && seg.header().syn) {
+        auto rd = get_random_generator();
+        _isn = WrappingInt32(rd());
+        _seq = seg.header().seqno;
+
+        auto data = seg.payload().copy();
+        auto eof = seg.header().fin;
+        _reassembler.push_substring(data, 0, eof);
+        
+        // SYN or FIN make _ackno+1
+        auto ctrl = seg.length_in_sequence_space() - seg.payload().size();
+        _ackno = WrappingInt32(_seq) + ctrl + _reassembler.first_unassembled();
+
+        return;
+    }
+
+    if (!_ackno.has_value() || stream_out().input_ended()) {
+        return;
+    }
+
+    auto data = Buffer(move(seg.payload().copy()));
+    auto index = unwrap(seg.header().seqno, _seq + 1, _checkpt);  // "+ 1" for the "SYN"
+    auto eof = seg.header().fin;
+
+    _reassembler.push_substring(data, index, eof);
+    _ackno = _ackno.value() + _reassembler.first_unassembled() - _checkpt;
+
+    // FIN should make _ackno + 1
+    if (stream_out().input_ended()) {
+        _ackno = _ackno.value() + 1;
+    }
+
+    _checkpt = _reassembler.first_unassembled();
 }
 
-optional<WrappingInt32> TCPReceiver::ackno() const { return {}; }
+optional<WrappingInt32> TCPReceiver::ackno() const { return _ackno; }
 
-size_t TCPReceiver::window_size() const { return {}; }
+size_t TCPReceiver::window_size() const { return stream_out().remaining_capacity(); }
--- libsponge/tcp_receiver.hh
+++ libsponge/tcp_receiver.hh
@@ -20,6 +20,12 @@ class TCPReceiver {
     //! The maximum number of bytes we'll store.
     size_t _capacity;
 
+    std::optional<WrappingInt32> _ackno{};
+    WrappingInt32 _isn{0};
+    WrappingInt32 _seq{0};
+    // the index of the last reassembled byte
+    uint64_t _checkpt{0};
+
   public:
     //! \brief Construct a TCP receiver
     //!
