05b18fce67c059733b91ded96c173b2e58297493
misaka <1784321894@qq.com>
2023-02-15 06:47:03 +0000

    CS144 Lab 2 tcp_receiver comment

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -9,44 +9,54 @@
 using namespace std;
 
 void TCPReceiver::segment_received(const TCPSegment &seg) {
-    if (!_ackno.has_value() && !seg.header().syn) {
+    /**
+     * @note Listen state
+     * @def not ackno().has_value() 
+     */
+    if (!_ackno.has_value()) {
+        // Handshake
+        if (seg.header().syn) {
+            auto rd = get_random_generator();
+            _isn = WrappingInt32(rd());
+            _seq = seg.header().seqno;
+
+            auto data = seg.payload().copy();
+            auto eof = seg.header().fin;
+            _reassembler.push_substring(data, 0, eof);
+            
+            // SYN or FIN make _ackno+1
+            auto ctrl = seg.length_in_sequence_space() - seg.payload().size();
+            _ackno = WrappingInt32(_seq) + ctrl + _reassembler.first_unassembled();
+        }
+
         return;
     } 
-    
-    // Handshake
-    if (!_ackno.has_value() && seg.header().syn) {
-        auto rd = get_random_generator();
-        _isn = WrappingInt32(rd());
-        _seq = seg.header().seqno;
 
-        auto data = seg.payload().copy();
-        auto eof = seg.header().fin;
-        _reassembler.push_substring(data, 0, eof);
-        
-        // SYN or FIN make _ackno+1
-        auto ctrl = seg.length_in_sequence_space() - seg.payload().size();
-        _ackno = WrappingInt32(_seq) + ctrl + _reassembler.first_unassembled();
-
+    /**
+     * @note FIN_RECV state
+     * @def stream_out.input_ended()
+     */
+    if (stream_out().input_ended()) {
         return;
     }
 
-    if (!_ackno.has_value() || stream_out().input_ended()) {
-        return;
-    }
-
+    /**
+     * @note SYN_RECV state
+     * @def ackno.has_value() and not stream_out.input_ended()
+     * @code 48 - 54
+     */
     auto data = Buffer(move(seg.payload().copy()));
     auto index = unwrap(seg.header().seqno, _seq + 1, _checkpt);  // "+ 1" for the "SYN"
     auto eof = seg.header().fin;
 
     _reassembler.push_substring(data, index, eof);
     _ackno = _ackno.value() + _reassembler.first_unassembled() - _checkpt;
+    _checkpt = _reassembler.first_unassembled();
 
     // FIN should make _ackno + 1
     if (stream_out().input_ended()) {
         _ackno = _ackno.value() + 1;
     }
-
-    _checkpt = _reassembler.first_unassembled();
 }
 
 optional<WrappingInt32> TCPReceiver::ackno() const { return _ackno; }
