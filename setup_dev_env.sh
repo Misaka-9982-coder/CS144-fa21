4fa5a00201d0808311ee7e61879a9a3402368b47
misaka <1784321894@qq.com>
2023-04-06 10:55:09 +0800

    chore: Remove GCC alternatives setup from dev environment script

--- setup_dev_env.sh
+++ setup_dev_env.sh
@@ -24,13 +24,3 @@ apt-get -y install build-essential gcc g++ cmake libpcap-dev htop jnettop screen
                    parallel iptables mahimahi mininet net-tools tcpdump wireshark telnet socat \
                    clang clang-format clang-tidy clang-tools coreutils bash doxygen graphviz   \
                    virtualbox-guest-utils netcat-openbsd
-
-## make a sane set of alternatives for gcc, and make gcc-8 the default
-# GCC
-update-alternatives --remove-all gcc &>/dev/null
-for ver in 7 8; do
-    update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-${ver} $((10 * ${ver})) \
-        $(for prog in g++ gcc-ar gcc-nm gcc-ranlib gcov gcov-dump gcov-tool; do
-            echo "--slave /usr/bin/${prog} ${prog} /usr/bin/${prog}-${ver}"
-        done)
-done
