a028eeb16d13d062770638f8f6c0576d4fdc95a0
misaka <1784321894@qq.com>
2023-04-06 11:01:34 +0800

    chore: Update development environment setup script

--- setup_dev_env.sh
+++ setup_dev_env.sh
@@ -13,8 +13,6 @@ apt-get -y install software-properties-common
 add-apt-repository multiverse
 add-apt-repository universe
 add-apt-repository restricted
-# Add mahimahi PPA
-add-apt-repository -y ppa:keithw/mahimahi
 
 ### make sure we're totally up-to-date now
 apt-get update
@@ -23,6 +21,6 @@ apt-get -y dist-upgrade
 ### install the software we need for the VM and build env
 apt-get -y install build-essential gcc g++ cmake libpcap-dev htop jnettop screen   \
                    emacs-nox vim-nox automake pkg-config libtool libtool-bin git tig links     \
-                   parallel iptables mahimahi mininet net-tools tcpdump wireshark telnet socat \
+                   parallel iptables mininet net-tools tcpdump wireshark telnet socat \
                    clang clang-format clang-tidy clang-tools coreutils bash doxygen graphviz   \
                    virtualbox-guest-utils netcat-openbsd
